da86fb417e65d2957735bcb37b534e2162d07bb8
raghad akash <raghad2462004.@gmail.com>
2026-04-08 15:03:33 +0300

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,6 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->group(function (){
-    //Route::apiResource('rooms',RoomController::class);
-    Route::apiResource('tables',TableController::class);
     Route::apiResource('packages',PackageController::class);
     Route::get('/bookings', [BookingController::class, 'index']);
     Route::post('/bookings', [BookingController::class, 'store']);
@@ -32,6 +30,30 @@ Route::middleware('auth:sanctum')->group(function (){
 
     Route::post('/rooms', [RoomController::class, 'store'])
         ->middleware('can:room.create');
+
+    Route::get('/rooms', [RoomController::class, 'show'])
+        ->middleware('can:room.show');
+
+    Route::put('/rooms', [RoomController::class, 'update'])
+        ->middleware('can:room.update');
+
+    Route::delete('/rooms', [RoomController::class, 'delete'])
+        ->middleware('can:room.delete');
+
+    Route::get('/tables', [TableController::class, 'index'])
+        ->middleware('can:table.index');
+
+    Route::post('/tables', [TableController::class, 'store'])
+        ->middleware('can:table.create');
+
+    Route::get('/tables', [TableController::class, 'show'])
+        ->middleware('can:table.show');
+
+    Route::put('/tables', [TableController::class, 'update'])
+        ->middleware('can:table.update');
+
+    Route::delete('/tables', [TableController::class, 'delete'])
+        ->middleware('can:table.delete');
 });
 
 
